7dd59fb851790ac343042ea5b3b91f3664f554ba
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-08-16 14:13:50 +0530

    changed the folder name of message features

--- GenAI/Project/chaigpt/features/messages/hooks/use-message.ts
+++ GenAI/Project/chaigpt/features/messages/hooks/use-message.ts
@@ -3,8 +3,12 @@
 import { queryKeys } from "@/features/conversation/utils/query-keys";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { createMessage, deleteMessage, listMessages, updateMessage } from "../actions/messages-actions"
-
+import {
+  createMessage,
+  deleteMessage,
+  listMessages,
+  updateMessage,
+} from "../actions/messages-actions";
 
 /** Load messages for one conversation. */
 export function useMessages(conversationId: string | undefined) {
